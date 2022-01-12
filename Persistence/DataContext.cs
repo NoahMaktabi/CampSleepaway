ab7ae06cca108c29518a1f77f4e1485ce67f91e8
Noah Maktabi <61517594+noah11m@users.noreply.github.com>
2022-01-12 10:28:24 +0100

    Ef installed

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CampSleepAway;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
