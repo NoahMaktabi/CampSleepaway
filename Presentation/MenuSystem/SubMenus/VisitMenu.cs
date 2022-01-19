a3c05a037fb681875cd42111fe44be79104897ff
Noah Maktabi <61517594+noah11m@users.noreply.github.com>
2022-01-19 14:22:53 +0100

    VisitMenu implemented

--- Presentation/MenuSystem/SubMenus/VisitMenu.cs
+++ Presentation/MenuSystem/SubMenus/VisitMenu.cs
@@ -1,6 +1,8 @@
 ﻿using Application;
 using Persistence;
+using System;
 using System.Threading.Tasks;
+using Presentation.Input;
 
 namespace Presentation.MenuSystem.SubMenus
 {
@@ -19,12 +21,41 @@ namespace Presentation.MenuSystem.SubMenus
             {
                 "Show all registered visits",
                 "Show a specific visit by ID",
-                "Add a new cabin",
-                "Add camper to cabin",
-                "Register a counselor to cabin",
-                "Change cabin counselor",
+                "Register a new visit",
                 "Return to main menu",
             };
+            var menu = new MenuBuilder(prompt, options);
+            var selectedIndex = menu.Run();
+            var result = "";
+            switch (selectedIndex)
+            {
+                case 0:
+                    result = await _handler.GetAllVisits();
+                    break;
+                case 1:
+                    result = await _handler.GetVisitById(InputHandler.GetIdFromUser("visit", "show"));
+                    break;
+                case 2:
+                    result = await RegisterVisit();
+                    break;
+                case 3:
+                    return false;
+            }
+            Console.WriteLine(result);
+
+            Console.WriteLine("Press any key to return to cabin menu...");
+            Console.ReadKey(true);
+            await this.Run();
+
+            return false;
+        }
+
+        private async Task<string> RegisterVisit()
+        {
+            var camperId = InputHandler.GetIdFromUser("camper", "register a visitor");
+            var visitorId = InputHandler.GetIdFromUser("next of kin", "register as visitor");
+            var visitLength = InputHandler.GetVisitLength();
+            return await _handler.RegisterVisit(camperId, visitorId, visitLength);
         }
     }
 }
