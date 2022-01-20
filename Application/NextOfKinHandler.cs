4f32a51f56775419f1cf0a0fa16968c781edecde
Noah Maktabi <61517594+noah11m@users.noreply.github.com>
2022-01-20 11:32:54 +0100

    Add camper to next of kin implemented in handler

--- Application/NextOfKinHandler.cs
+++ Application/NextOfKinHandler.cs
@@ -9,9 +9,11 @@ namespace Application
     public class NextOfKinHandler
     {
         private readonly NextOfKinRepository _repository;
+        private readonly CamperRepository _camperRepository;
         public NextOfKinHandler(DataContext context)
         {
             _repository = new NextOfKinRepository(context);
+            _camperRepository = new CamperRepository(context);
         }
 
         public async Task<string>GetAllNextOfKins()
@@ -39,6 +41,24 @@ namespace Application
             return result
                 ? $"The next of kin {nextOfKin.Name} has been added to the database"
                 : "There was a problem, the next of kin was not added to the database";
+        }
+
+        public async Task<string> AddCamperToNextOfKin(int camperId, int nextOfKinId)
+        {
+            var camper = await _camperRepository.FindById(camperId);
+            if (camper == null) return "The camper was not found in the database";
+
+            var nextOfKin = await _repository.FindById(nextOfKinId);
+            if (nextOfKin == null) return "The next of kin was not found in the database";
+
+            if (nextOfKin.Camper != null)
+                return "The next of kin has already a camper assigned. This can not be changed!";
+
+            nextOfKin.Camper = camper;
+            var result = await _repository.Update(nextOfKin);
+            return result
+                ? "The camper has been added to the next of kin"
+                : "There was a problem. The camper was not added to the next of kin";
         }
 
         public async Task<string> UpdateNextOfKin(NextOfKin nextOfKin)
